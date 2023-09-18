814895492e42d1da3af80b760357d81a7319b64f
My Quyen <phanthimyquyen1004@gmail.com>
2023-09-18 23:42:21 +0700

    add form fRenuve(form quản lý doanh thu)

---
+++ Coffee Management/fRevenue.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coffee_Management
+{
+    public partial class fRevenue : DevExpress.XtraEditors.XtraForm
+    {
+        public fRevenue()
+        {
+            InitializeComponent();
+        }
+    }
+}
